1ac0d65067024ff93b5c531ea82dcd3c295b3a9a
Maya Kalitseva <maya.kalitseva@joy-dev.com>
2022-06-08 21:07:10 +0300

    no message

---
+++ Teleport/Extensions/Reusable.swift
@@ -0,0 +1,20 @@
+//
+//  Reusable.swift
+//  
+//
+//
+//
+
+import UIKit
+
+protocol Reusable: AnyObject {
+    static var reuseIdentifier: String { get }
+}
+
+extension Reusable {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UIView: Reusable { }
---
+++ Teleport/Extensions/UICollectionViewCell+Reusable.swift
@@ -0,0 +1,13 @@
+//
+//  UICollectionViewCell+Reusable.swift
+//  
+//
+//
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+
+    static var reuseIdentifier: String { String(describing: Self.self) }
+}
